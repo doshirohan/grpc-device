3f395fdefccd5ebe28bb51c585b7e318b596b253
Palash Khare <palash.khare@ni.com>
2021-04-26 16:49:39 +0530

    implementation

--- examples/nidcpower/MeasureRecord.py
+++ examples/nidcpower/MeasureRecord.py
@@ -20,12 +20,12 @@
 
 import grpc
 import sys
+import time
 import nidcpower_pb2 as nidcpower_types
 import nidcpower_pb2_grpc as grpc_nidcpower
-# import matplotlib.pyplot as plt
-# import keyword
-# import session_pb2 as session_types
-# import session_pb2_grpc as grpc_session
+import matplotlib.pyplot as plt
+import keyword
+import numpy as np
 
 server_address = "localhost"
 server_port = "31763"
@@ -37,9 +37,7 @@ options = "Simulate=1,DriverSetup=Model:4147;BoardType:PXIe"
 channels = "0"
 
 # Parameters
-MeasureWhen_value = nidcpower_types.MeasureWhen.MEASURE_WHEN_NIDCPOWER_VAL_AUTOMATICALLY_AFTER_SOURCE_COMPLETE
-MeasureRecordLength_value = 20
-MeasureRecordLengthIsFinite_value = True
+record_length = 100
 voltage_level = 5.0
 
 # Read in cmd args
@@ -82,55 +80,33 @@ try :
     ))
     vi = initialize_with_independent_channels_response.vi
     CheckForError(vi, initialize_with_independent_channels_response.status)
-    print("Initialize_with_independent_channels works fine")
 
     # configure measure_when
     configure_measure_when = client.SetAttributeViInt32(nidcpower_types.SetAttributeViInt32Request(
         vi = vi,
         channel_name = channels,
         attribute_id = nidcpower_types.NiDCPowerAttributes.NIDCPOWER_ATTRIBUTE_MEASURE_WHEN,
-        attribute_value = MeasureWhen_value
+        attribute_value = nidcpower_types.MeasureWhen.MEASURE_WHEN_NIDCPOWER_VAL_AUTOMATICALLY_AFTER_SOURCE_COMPLETE
     ))
     CheckForError(vi, configure_measure_when.status)
-    print("Configured measure_when attribute")
 
     # configure measure_record_length
     configure_measure_record_length = client.SetAttributeViInt32(nidcpower_types.SetAttributeViInt32Request(
         vi = vi,
         channel_name = channels,
         attribute_id = nidcpower_types.NiDCPowerAttributes.NIDCPOWER_ATTRIBUTE_MEASURE_RECORD_LENGTH,
-        attribute_value = MeasureRecordLength_value
+        attribute_value = record_length
     ))
     CheckForError(vi, configure_measure_record_length.status)
-    print("configured measure_record_length attribute")
-
-    # value of measure_record_length
-    get_value_measure_record_length = client.GetAttributeViInt32(nidcpower_types.GetAttributeViInt32Request(
-        vi = vi,
-        channel_name = channels,
-        attribute_id = nidcpower_types.NiDCPowerAttributes.NIDCPOWER_ATTRIBUTE_MEASURE_RECORD_LENGTH
-    ))
-    CheckForError(vi, get_value_measure_record_length.status)
-    print(get_value_measure_record_length.attribute_value)
-
+  
     # configure measure_record_length_is_finite
     configure_measure_record_length_is_finite = client.SetAttributeViBoolean(nidcpower_types.SetAttributeViBooleanRequest(
         vi = vi,
         channel_name = channels,
         attribute_id = nidcpower_types.NiDCPowerAttributes.NIDCPOWER_ATTRIBUTE_MEASURE_RECORD_LENGTH_IS_FINITE,
-        attribute_value = MeasureRecordLengthIsFinite_value
+        attribute_value = False
     ))
     CheckForError(vi, configure_measure_record_length_is_finite.status)
-    print("configured measure_record_length_is_finite attribute")
-
-    # value of measure_record_length_is_finite
-    get_value_measure_record_length_is_finite = client.GetAttributeViBoolean(nidcpower_types.GetAttributeViBooleanRequest(
-        vi = vi,
-        channel_name = channels,
-        attribute_id = nidcpower_types.NiDCPowerAttributes.NIDCPOWER_ATTRIBUTE_MEASURE_RECORD_LENGTH_IS_FINITE
-    ))
-    CheckForError(vi, get_value_measure_record_length_is_finite.status)
-    print(get_value_measure_record_length_is_finite.attribute_value)
 
     # configure voltage level
     configure_voltage_level = client.ConfigureVoltageLevel(nidcpower_types.ConfigureVoltageLevelRequest(
@@ -139,7 +115,6 @@ try :
         level = voltage_level
     ))
     CheckForError(vi, configure_voltage_level.status)
-    print("Configured voltage level")
 
     # commit the session
     commit_with_channels = client.CommitWithChannels(nidcpower_types.CommitWithChannelsRequest(
@@ -147,7 +122,6 @@ try :
         channel_name = channels
     ))
     CheckForError(vi, commit_with_channels.status)
-    print("Commit_with_channels works fine!")
 
     # get measure_record_delta_time
     get_measure_record_delta_time = client.GetAttributeViReal64(nidcpower_types.GetAttributeViReal64Request(
@@ -156,36 +130,60 @@ try :
        attribute_id =  nidcpower_types.NiDCPowerAttributes.NIDCPOWER_ATTRIBUTE_MEASURE_RECORD_DELTA_TIME
     ))
     CheckForError(vi, get_measure_record_delta_time.status)
-    print(f"Effective measurement rate : {get_measure_record_delta_time.attribute_value}")
-
+    
     # initiate the session
-    initiate = client.Initiate(nidcpower_types.InitiateRequest(
-        vi = vi
+    initiate_with_channels = client.InitiateWithChannels(nidcpower_types.InitiateWithChannelsRequest(
+        vi = vi,
+        channel_name = channels
     ))
-    CheckForError(vi, initiate.status)
-    print("Session initiated")
+    CheckForError(vi, initiate_with_channels.status)
+    
+    # Setup a plot to draw the captured waveform
+    fig = plt.figure("Waveform Graph")
+    fig.show()
+    fig.canvas.draw()
 
-    samples_aquired = 0
-    while (samples_aquired < 20):
+    # Handle closing of plot window
+    closed = False
+    def on_close(event):
+        global closed
+        closed = True
+    fig.canvas.mpl_connect('close_event', on_close)
 
-        fetch_multiple_response = client.FetchMultiple(nidcpower_types.FetchMultipleRequest(
-            vi = vi,
-            channel_name = channels,
-            timeout = 5,
-            count = 20
-        ))
-        CheckForError(vi, fetch_multiple_response.status)
-        print("FetchMultiple API works fine")
-        samples_aquired += fetch_multiple_response.actual_count
-        
+    x_axis = np.arange(start=1, stop=record_length+1, step=1)
 
+    print("\nReading values in loop. Close window to stop.\n")
+
+    try:
+        while not closed:
+            # Clear the plot and setup the axis
+            plt.clf()
+            plt.axis()
+            plt.xlabel("Time")
+            plt.ylabel("Amplitude")
+            
+            fetch_multiple_response = client.FetchMultiple(nidcpower_types.FetchMultipleRequest(
+                vi = vi,
+                channel_name = channels,
+                timeout = 0.001,
+                count = record_length
+            ))
+            CheckForError(vi, fetch_multiple_response.status)
+            y_axis = fetch_multiple_response.voltage_measurements
+     
+            plt.plot(x_axis, y_axis)
+            plt.pause(0.001)
+            x_axis += record_length
+
+    except KeyboardInterrupt:
+        pass
+
+    print(f"Effective measurement rate : {get_measure_record_delta_time.attribute_value}")
 
     # close API
     CheckForError(vi, (client.Close(nidcpower_types.CloseRequest(
         vi = vi
         ))).status)
-    print("Close API works fine")
-    # channel.close()
 
 except grpc.RpcError as rpc_error:
     error_message = rpc_error.details()
