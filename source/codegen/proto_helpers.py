fb0862799aa4fc2c6877d76420c3ca68ff5f2082
Palash Khare <palash.khare@ni.com>
2021-05-12 17:21:26 +0530

    adding support for ViUInt8 array

--- source/codegen/proto_helpers.py
+++ source/codegen/proto_helpers.py
@@ -43,7 +43,11 @@ def get_grpc_type_from_ivi(type, is_array, driver_name_pascal):
   if 'ViUInt64' in type:
     type = 'uint64'
   if 'ViUInt8' in type:
-    type = 'uint32'
+    if is_array == True:
+      type = "bytes"
+      add_repeated = False
+    else:
+      type = 'uint32'
   if 'ViStatus' in type:
     type = 'sint32'
   if 'ViAddr' in type:
